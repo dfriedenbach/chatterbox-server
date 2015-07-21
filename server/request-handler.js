5c376a577843872f3ff68db1eddd7cb600ec1c00
dfriedenbach <danielfriedenbach@gmail.com>
2015-07-21 10:25:17 -0700

    Server generates timestamp and random ID for new messages

--- server/request-handler.js
+++ server/request-handler.js
@@ -9,6 +9,18 @@ var data = {
     ] // implement max 100 messasges
   };
 
+var generateRandomId = function () {
+  var letters = [
+    1,2,3,4,5,6,7,8,9,0,
+    'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+    'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+  ];
+  for (var i = 0, id = ""; i < 10; i++) {
+    id += letters[Math.floor(Math.random() * letters.length)];
+  }
+  return id;
+};
+
 var requestHandler = function(request, response) {
   var statusCode = 200;
   var headers = defaultCorsHeaders;
@@ -23,12 +35,10 @@ var requestHandler = function(request, response) {
       var body = "";
       request.on('data', function (message) {
         var message = JSON.parse(message);
+        message.createdAt = new Date().toISOString();
+        message.objectId = generateRandomId();
         data.results.push(message);
-        //body += message;
       });
-      // request.on('end', function () {
-      //   data.results.push(JSON.parse(body));
-      // });
       response.end('{"status":200}');
     } else if (request.method === 'GET') { // send data
       response.end(JSON.stringify(data), 'utf8', function () { console.log('Messages Sent'); });
